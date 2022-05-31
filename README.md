aca4e5ee57ba53f09035ce8b69a5b457a56bc259
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-05-31 15:49:57 +0200

    app working correctly with local input(json) and output(parquet)

--- README.md
+++ README.md
@@ -1,33 +1,39 @@
-# Exercise: Developing Kafka Producer
+# Challenge: 1: Spark, Kafka, Hadoop
 
-## How to run (Windows)
+## Task:
+```text
+Write a Spark application that reads records from a Kafka topic.
+This application does the following transformations:
 
-- zookeeper:
+Writes records to Kafka in JSON format
+JSON example:
+{
+    "name": "John",
+    "surname": "Doe",
+    "cats": 1,
+    "dogs": 2
+}
+Reads the data from the topic
+Parses the JSONs
+Surname to upper case letters
+Sum cats and dogs fields and write the result to the new field “animals”.
+The result should be written as a parquet file in which every record consists of 3 columns: name (String), surname (String), animals (Integer).
 
-````
-    cd C:\kafka\bin\windows
-    zookeeper-server-start.bat C:\kafka\config\zookeeper.properties
-````
+How to parse JSON in Scala:
+https://alvinalexander.com/scala/how-to-create-scala-object-instance-from-json-string/
+https://sparkbyexamples.com/spark/spark-parse-json-from-text-file-string/
 
-- kafka server:
+Choose the method which suits you best.
 
-````
-    cd C:\kafka\bin\windows
-    kafka-server-start.bat C:\kafka\config\server.properties
-````
+Expected results
+The result should be saved to an HDFS location.
+Deploy this Spark application to a YARN cluster.
+Write tests.
+Don’t forget about README with proper structure
+Publish to a github repo
 
-- read message:
+```
 
- ````
-    cd C:\kafka\bin\windows
-    kafka-console-consumer.bat --topic output --from-beginning --bootstrap-server localhost:9092
- ````
-- write message:
-
- ````
-    cd C:\kafka\bin\windows
-    kafka-console-producer.bat --topic input --bootstrap-server localhost:9092
- ````
 ## How to run (macOS)
 
 ````
@@ -54,4 +60,8 @@
     cd kafka
     ./bin/kafka-console-producer.sh --topic input --bootstrap-server localhost:9092
  ````
+
+## Deploying on Yarn
+In terminal go to the Spark directory and run command
+```./bin/spark-submit --class <classname> --master yarn \ --<jarpath> args   ```
 
--- src/main/scala/CatsAndDogs.scala
+++ src/main/scala/CatsAndDogs.scala
@@ -1,47 +1,47 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.upper
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
 object CatsAndDogs {
-  def main(args: Array[String]):Unit = {
+
+  def main(args: Array[String]): Unit = {
     import org.apache.spark.sql.SparkSession
-
     val path = if (args.length > 0) args(0)
-    else "src/main/resources/input.json"
+    else "src/main/resources/json"
 
-    println(s"loading files from: $path")
-
-    implicit val spark = SparkSession
+    val spark = SparkSession
       .builder()
       .master("local[*]")
       .getOrCreate()
+    import spark.implicits._
 
-    val message = spark
-      .readStream
-      .format()
-      .option("subscribe", "cats")
-/*
-      .option("kafka.bootstrap.servers", ":9092")
-*/
-      .option("includeHeaders", "true")
-      .load
-      .show()
-    /*val result = message.toDF()
-      .withColumn("")*/
-    // val output = loadFilesFromPath(path)
-  }
-
-/*  val finalMessage = upperMessage
-    .writeStream
-    .format("kafka")
-    .option("topic", "animals")
-    .option("checkpointLocation", "checkpoint")
-    .option("kafka.bootstrap.servers", ":9092")
-    .start()
-    .awaitTermination*/
-
-
-  def loadFilesFromPath(path: String) (implicit spark: SparkSession) = {
-    spark
+    val input = spark
       .read
-      .text(path)
+      .option("multiline", "true")
+      .option("inferSchema", "true")
+      .json(path)
+    input.printSchema()
+
+    val newTable = input
+      .withColumn("surname", upper($"surname"))
+      .withColumn("animals", $"cats" + $"dogs")
+      .drop($"cats")
+      .drop($"dogs")
+
+    val result = newTable
+      .write
+      .parquet("C:\\exam\\challenge-first\\parquet")
+
+    /*val djJSON = input.withColumn("value", from_json(col("value"), schema))
+      .select("jsonData.*")
+
+
+    */
+    // Write back
+    /* .write
+    .
+     .format("kafka")
+     .option("kafka.bootstrap.servers", "localhost:9092")
+     .option("subscribe", "outputTopic")
+     .start()*/
   }
 }
